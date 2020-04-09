28bdacef996e3390b6fd08064c085533e3cbe110
Matias Manavella <admin@Administrators-MacBook-Pro.local>
2020-04-09 17:19:08 -0300

    Config added

---
+++ models/Config.go
@@ -0,0 +1,11 @@
+package models
+
+//Config parses the config.json file
+type Config struct {
+	ServerPort   string
+	MySQLUser    string
+	MySQLPass    string
+	MySQLDB      string
+	DBSampleUser string
+	DBSamplePass string
+}
