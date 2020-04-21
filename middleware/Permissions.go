a240c69afefd05c3de9f1bb2e2d562a0f52d7287
Matias Manavella <admin@Administrators-MacBook-Pro.local>
2020-04-21 16:51:35 -0300

    Modified CheckPermissions

--- middleware/Permissions.go
+++ middleware/Permissions.go
@@ -2,9 +2,9 @@ package middleware
 
 import "github.com/gin-gonic/gin"
 
-//Checks if user has the required permission for certain endpoints. 
-func CheckPermissions(){
-	return func (c *gin.Context){
+//CheckPermissions checks if user has the required permission for certain endpoints.
+func CheckPermissions() gin.HandlerFunc {
+	return func(c *gin.Context) {
 
 	}
 }
