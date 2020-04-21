d5bce483f9a198cd4a6d5561f2014db02d9cb558
Matias Manavella <admin@Administrators-MacBook-Pro.local>
2020-04-21 16:45:28 -0300

    added Permission.go

---
+++ middleware/Permissions.go
@@ -0,0 +1,10 @@
+package middleware
+
+import "github.com/gin-gonic/gin"
+
+//Checks if user has the required permission for certain endpoints. 
+func CheckPermissions(){
+	return func (c *gin.Context){
+
+	}
+}
